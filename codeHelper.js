a92e9d6bb0fe4da600ffe3d49f8c84ac9c3a51d0
sripathi121 <157799338+sripathi121@users.noreply.github.com>
2024-01-26 16:15:22 +0530

    Add files via upload

---
+++ codeHelper.js
@@ -0,0 +1,20 @@
+codeHelper.js
+const fs = require('fs');
+
+module.exports = {
+    createDir: dir => {
+        fs.mkdirSync(`./${dir}`);
+    },
+    createFile: (path, ext, content) => {
+        fs.writeFileSync(path + ext, content)
+    },
+    removeDir: dir => {
+        const files = fs.readdirSync(`./${dir}`);
+
+        files.forEach(file => {
+            fs.unlinkSync(`./${dir}/${file}`);
+        });
+
+        fs.rmdirSync(`./${dir}`);
+    }
+};
